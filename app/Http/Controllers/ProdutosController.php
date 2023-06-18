41f557d770f06ef701096e3fddfdcbb1afaf7a74
Sandro <sandrolor@gmail.com>
2023-06-18 15:01:58 -0300

    referenciar rota front

---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutosController extends Controller
+{
+    //Paginação dos dados
+    public function index(){
+        return 'produtos';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::prefix('produtos')->group(function(){
+    Route::get('/', [ProdutosController::class, 'index'])->name('produto.index');
 });
