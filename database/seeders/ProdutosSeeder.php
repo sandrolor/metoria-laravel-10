7ab57d60cc5c3a0d57fbdf30c3e84f6047fc9f46
Sandro <sandrolor@gmail.com>
2023-06-18 17:31:14 -0300

    paginacao produto

---
+++ database/seeders/ProdutosSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Produto;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProdutosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Produto::create([
+            'nome' => 'Sandro Luiz',
+            'valor' => '20.00'
+        ]);
+    }
+}
---
+++ resources/views/pages/produtos/paginacao.blade.php
@@ -0,0 +1,34 @@
+@extends('index')
+@section('content')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Produtos</h1>
+</div>
+<div>
+    <form action="" method="GET" class="form-inline">
+        <div class="form-group mx-sm-3 mb-2">
+            <input type="text" class="form" name="pesquisar" id="pesquisar" placeholder="Digite o nome" />
+            <button class="btn btn-secondary">Pesquisar</button>
+            <a href="" type="button" class="btn btn-success float-end">Novo Produto</a>
+        </div>
+    </form>
+    <div class="table-responsive small mt-4">
+        <table class="table table-striped table-sm">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Valor</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($findProduto as $produto)
+                    <tr>
+                        <td>{{$produto->nome}}</td>
+                        <td>{{ 'R$' . ' ' . number_format($produto->valor, 2, ',', '.') }}</td>
+                        <td><a href="" class="btn btn-light btn-sm">Editar</a>  | <a href="" class="btn btn-danger btn-sm">Excluir</a></td>
+                    </tr>    
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
